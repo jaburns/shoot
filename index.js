b056b9fa28c79e8f51ff1173b8ef6c5f42349dc7
Jeremy Burns <j@jaburns.net>
2014-08-05 14:29:26 -0600

    Fixed index to server files from www only

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ var port = process.argv.length > 2
 
 var server = require ('http').createServer (handler);
 var fs = require ('fs');
+var path = require ('path');
+
+var wwwPrefix = __dirname + '/www/';
 
 function handler (req, res) {
-  var filepath = req.url;
+  var requestPath = req.url;
+  if (requestPath === '/') requestPath = '/index.html';
+  var normalizedPath = path.normalize (wwwPrefix + requestPath);
 
-  if (filepath === '/') filepath = '/index.html';
-  filepath = '/www/' + filepath;
-
-  if (FILES.indexOf (filepath) < 0) {
+  if (normalizedPath.indexOf (wwwPrefix) !== 0) {
+    res.writeHead (403);
+    return res.end ('403');
+  } else if (! fs.existsSync (normalizedPath)) {
     res.writeHead (404);
     return res.end ('404');
   }
 
-  fs.readFile (__dirname + filepath, function (err, data) {
+  fs.readFile (normalizedPath, function (err, data) {
     if (err) {
       res.writeHead (500);
       return res.end ('500');
@@ -29,5 +34,6 @@ function handler (req, res) {
 }
 
 var game = require ('./www/game.js');
-require ('gamesync').run (server, game, lag);
+require ('gamesync').run (server, game);
 server.listen (port);
+
