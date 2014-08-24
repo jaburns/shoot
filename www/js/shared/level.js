66848b1de196baf203c9a7fb6bb5dc8661ef0d83
Jeremy Burns <j@jaburns.net>
2014-08-24 17:49:07 -0600

    Removed collision line gen from Level

--- www/js/shared/level.js
+++ www/js/shared/level.js
@@ -11,9 +11,6 @@ define(function(require) {
     this.solids = data.solids;
   }
 
-  var SEG_LENGTH = 50;
-  var LEN_MEASURE_PRECISION = 20;
-
   Level.prototype.getCurves = function (solidIndex) {
     var solid = this.solids[solidIndex];
     var ret = [[ solid[0] ]];
@@ -34,26 +31,6 @@ define(function(require) {
     }
 
     return ret;
-  }
-
-  Level.prototype.getLines = function () {
-    var curves = this.getCurves(0);
-    var pts = [curves[0][0]];
-
-    for (var i = 1; i < curves.length; ++i) {
-      if (curves[i].length < 2) {
-        pts.push (curves[i][0]);
-        continue;
-      }
-      var ptsIn = [ curves[i-1][curves[i-1].length-1] ].concat (curves[i]);
-      var len = geom.bezierLength (ptsIn, LEN_MEASURE_PRECISION);
-      var segs = ~~(len / SEG_LENGTH);
-      for (var j = 1; j <= segs; ++j) {
-        pts.push (geom.bezier (ptsIn, j / segs));
-      }
-    }
-
-    return pts;
   }
 
   return Level;
