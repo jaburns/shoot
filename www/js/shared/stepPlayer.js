2305c3d829008ec648d5383afe5c30f440e3b069
Jeremy Burns <j@jaburns.net>
2014-08-24 23:47:33 -0600

    Very crude buggy 2p collision for now

--- www/js/shared/stepPlayer.js
+++ www/js/shared/stepPlayer.js
@@ -6,6 +6,7 @@ define(function(require) {
   'use strict'
 
   var levelData = require('./levelData');
+  var Vec2 = require('./vec2');
 
   return function (input, player, state) {
     var level = levelData[state.level];
@@ -30,6 +31,22 @@ define(function(require) {
 
     player.wheel.omega = player.vel.x / 15;
     player.wheel.theta += player.wheel.omega;
+
+    if (player !== state.players[0]) {
+      for (var i = 0; i < state.players.length; i++) {
+        var p = state.players[i];
+        if (p === player) continue;
+        var d = new Vec2 (player.pos.x - p.pos.x, player.pos.y - p.pos.y);
+        if (d.magnitude2() < 30*30) {
+          var dmag = d.magnitude();
+          d.normalize().scale(15 - 0.5*dmag);
+          player.pos.x += d.x;
+          player.pos.y += d.y;
+          p.pos.x -= d.x;
+          p.pos.y -= d.y;
+        }
+      }
+    }
 
     var collided = level.collision.step (player.pos, player.vel, 15);
 
